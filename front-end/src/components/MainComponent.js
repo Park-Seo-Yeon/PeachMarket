98333bae7a6fefb6898e79395f7e915546a5b1b1
kimtaekyung <ktk8916@naver.com>
2022-04-11 13:40:16 +0900

    메인, 카테고리

--- front-end/src/components/MainComponent.js
+++ front-end/src/components/MainComponent.js
@@ -3,49 +3,35 @@ import { Link } from "react-router-dom";
 import styles from "./Main.module.css";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { Container, Col, Row } from "react-bootstrap";
+import exdata from "./exdata.json";
+import useStore from "./useStore"
 
 function MainComponent() {
   const [products, setProducts] = useState([]);
-  const [category, setCategory] = useState("");
-  useEffect(() => {}, []);
+  const [cateProducts, setCateProducts] = useState([]);
+  const {category, setCategory} = useStore();
+  
+  useEffect(() => {
+    setProducts(exdata.product);
+    setCateProducts(exdata.product);
+    changeCategory(category);
+  }, [category]);
+
+  const changeCategory = (category) => {
+    if(category == "전체"){
+      setCateProducts(exdata.product);
+    }
+    else{
+      setCateProducts(products.filter(d => d.category == category));
+    }
+    
+}
+ 
   return (
     <div className={styles.container}>
       <Container>
         <Row>
-          <Col lg={6}>
-            <Link to={`./products/${1}`}>
-              <div className={styles.product_container}>
-                <img
-                  className={styles.product_img}
-                  src="https://peachmarket-2022-bucket.s3.ap-northeast-2.amazonaws.com/clothes1.png"
-                  alt=""
-                ></img>
-                <div className={styles.product_info}>
-                  <p className={styles.product_title}>후리스</p>
-                  <p className={styles.product_time}>3분 전</p>
-                  <p className={styles.product_price}>30,000원</p>
-                </div>
-              </div>
-            </Link>
-          </Col>
-          <Col lg={6}>
-            <Link to={"./products/1"}>
-              <div className={styles.product_container}>
-                <img
-                  className={styles.product_img}
-                  src="https://peachmarket-2022-bucket.s3.ap-northeast-2.amazonaws.com/clothes1.png"
-                  alt=""
-                ></img>
-                <div className={styles.product_info}>
-                  <p className={styles.product_title}>후리스</p>
-                  <p className={styles.product_time}>3분 전</p>
-                  <p className={styles.product_price}>30,000원</p>
-                </div>
-              </div>
-            </Link>
-          </Col>
-          
- {/*          {products.map((product) => {
+          {cateProducts.map((product) => (
             <Col lg={6} key={product.productId}>
               <Link to={`./products/${product.productId}`}>
                 <div className={styles.product_container}>
@@ -61,40 +47,8 @@ function MainComponent() {
                   </div>
                 </div>
               </Link>
-            </Col>;
-          })} */}
-          <Col lg={6}>
-            <Link to={`./products/${1}`}>
-              <div className={styles.product_container}>
-                <img
-                  className={styles.product_img}
-                  src="https://peachmarket-2022-bucket.s3.ap-northeast-2.amazonaws.com/clothes1.png"
-                  alt=""
-                ></img>
-                <div className={styles.product_info}>
-                  <p className={styles.product_title}>후리스</p>
-                  <p className={styles.product_time}>3분 전</p>
-                  <p className={styles.product_price}>30,000원</p>
-                </div>
-              </div>
-            </Link>
-          </Col>
-          <Col lg={6}>
-            <Link to={`./products/${1}`}>
-              <div className={styles.product_container}>
-                <img
-                  className={styles.product_img}
-                  src="https://peachmarket-2022-bucket.s3.ap-northeast-2.amazonaws.com/clothes1.png"
-                  alt=""
-                ></img>
-                <div className={styles.product_info}>
-                  <p className={styles.product_title}>후리스</p>
-                  <p className={styles.product_time}>3분 전</p>
-                  <p className={styles.product_price}>30,000원</p>
-                </div>
-              </div>
-            </Link>
-          </Col>
+            </Col>
+          ))}
         </Row>
       </Container>
 
