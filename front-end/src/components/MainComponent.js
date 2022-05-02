457ff223e5d5c0d8ff8a249c4385d852e066eab6
kimtaekyung <ktk8916@naver.com>
2022-05-02 13:11:21 +0900

    글 작성

--- front-end/src/components/MainComponent.js
+++ front-end/src/components/MainComponent.js
@@ -10,16 +10,16 @@ import ProductService from "../service/ProductService";
 function MainComponent() {
   const [products, setProducts] = useState([]);
   const [cateProducts, setCateProducts] = useState([]);
+  const [search, setSearch] = useState("");
   const { category, setCategory } = useStore();
 
-  
   useEffect(() => {
     setProducts(exdata.product);
     setCateProducts(exdata.product);
     changeCategory(category);
   }, [category]);
-  
-/*
+
+  /*
   useEffect(() => {
     ProductService.getProducts().then((res) => {
       setProducts(res.data);
@@ -30,7 +30,7 @@ function MainComponent() {
   */
 
   const changeCategory = (category) => {
-    if (category == "전체") {
+    if (category == "0") {
       setCateProducts(exdata.product);
     } else {
       setCateProducts(products.filter((d) => d.category == category));
@@ -40,25 +40,46 @@ function MainComponent() {
   return (
     <div className={styles.container}>
       <Container>
+        <div className="text-center my-5">
+          <input
+            type="text"
+            placeholder="search..."
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
+        </div>
         <Row>
-          {cateProducts.map((product) => (
-            <Col lg={6} key={product.productId}>
-              <Link to={`./products/${product.productId}`}>
-                <div className={styles.product_container}>
-                  <img
-                    className={styles.product_img}
-                    src={product.productImg}
-                    alt=""
-                  ></img>
-                  <div className={styles.product_info}>
-                    <p className={styles.product_title}>{product.title}</p>
-                    <p className={styles.product_time}>{product.createTime}</p>
-                    <p className={styles.product_price}>{product.price}</p>
+          {cateProducts
+            .filter((item) => {
+              if (search === "") {
+                return item;
+              } else if (
+                item.title.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return item;
+              }
+            })
+            .map((product) => (
+              <Col lg={6} key={product.productId}>
+                <Link to={`./products/${product.productId}`}>
+                  <div className={styles.product_container}>
+                    <img
+                      className={styles.product_img}
+                      src={product.productImg}
+                      alt=""
+                    ></img>
+                    <div className={styles.product_info}>
+                      <p className={styles.product_title}>{product.title}</p>
+                      <p className={styles.product_time}>
+                        {product.createTime}
+                      </p>
+                      <p className={styles.product_price}>{product.price}</p>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </Col>
-          ))}
+                </Link>
+              </Col>
+            ))}
         </Row>
       </Container>
 
