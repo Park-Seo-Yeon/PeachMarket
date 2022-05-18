0f71d81ca0afd35cc0582496e69a6cedaede5af9
KimSuhyun <suhun1054@gmail.com>
2022-05-18 16:35:15 +0900

    change save path

--- Fitting/flaskFitting.py
+++ Fitting/flaskFitting.py
@@ -8,9 +8,8 @@ CORS(app, resources={r'*': {'origins': '*'}})
 
 @app.route('/fitting_cpVtonPlus/<id>', methods=['GET', 'POST'])
 def flaskFitting(id):
-    # fittingMain()
-    # return "https://peachmarket-bucket.s3.ap-northeast-2.amazonaws.com/data/result/TOM/try_on/" + id + ".jpg"
-    return id
+    fittingMain()
+    return "https://peachmarket-bucket.s3.ap-northeast-2.amazonaws.com/data/result/TOM/try_on/" + id + ".jpg"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.2", port="5000")
