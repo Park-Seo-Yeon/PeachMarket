3bda85c8a6baff55cf3db63c969ca6955072bdf9
1eejisoo <jisu991012@gmail.com>
2022-05-26 00:24:45 +0900

    s3 config

--- Back-End/src/main/java/com/market/repository/ChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.market.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.market.entity.ChatRoom;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Integer>  {
-
-}
